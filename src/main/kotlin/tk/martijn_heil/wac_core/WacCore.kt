a5bd9027df500e60bd8bb2098e58c257559ec4f8
martijn-heil <m.heil375@gmail.com>
2016-10-03 17:58:33 +0200

    Better exception handling in WacCore#onEnable

--- src/main/kotlin/tk/martijn_heil/wac_core/WacCore.kt
+++ src/main/kotlin/tk/martijn_heil/wac_core/WacCore.kt
@@ -3,9 +3,11 @@ package tk.martijn_heil.wac_core
 import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
 import org.flywaydb.core.Flyway
+import org.flywaydb.core.api.FlywayException
 import tk.martijn_heil.wac_core.itemproperty.ItemPropertyListener
 import java.sql.Connection
 import java.sql.DriverManager
+import java.sql.SQLException
 import java.util.*
 
 class WacCore : JavaPlugin() {
@@ -15,16 +17,29 @@ class WacCore : JavaPlugin() {
 
         val dbUrl = config.getString("db.url");
         val dbUsername = config.getString("db.username");
-        val dbPassword = config.getString("db.password")
+        val dbPassword = config.getString("db.password");
         // Storing the password in a char array doesn't improve much..
         // it's stored in plaintext in the "config" object anyway.. :/
 
         val flyway = Flyway();
         flyway.setDataSource(dbUrl, dbUsername, dbPassword);
         flyway.classLoader = this.classLoader;
-        flyway.migrate();
+        try {
+            flyway.migrate();
+        } catch (ex: FlywayException) {
+            logger.severe(ex.message);
+            this.isEnabled = false;
+            return;
+        }
 
-        dbconn = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
+        try {
+            dbconn = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
+        } catch (ex: SQLException) {
+            logger.severe(ex.message);
+            this.isEnabled = false;
+            return;
+        }
+
         messages = ResourceBundle.getBundle("messages.messages");
 
         Bukkit.getPluginManager().registerEvents(ItemPropertyListener(), this);
