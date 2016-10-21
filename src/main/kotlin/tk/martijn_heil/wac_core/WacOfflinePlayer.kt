e81ba10b53b93d0d0d073365f6f5a03f70203c0c
Martijn Heil <m.heil375@gmail.com>
2016-10-21 18:01:05 +0200

    Change license to GPL since Bukkit is GPL

--- src/main/kotlin/tk/martijn_heil/wac_core/WacOfflinePlayer.kt
+++ src/main/kotlin/tk/martijn_heil/wac_core/WacOfflinePlayer.kt
@@ -1,3 +1,21 @@
+/*
+ *     wac-core
+ *     Copyright (C) 2016 Martijn Heil
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package tk.martijn_heil.wac_core;
 
 import org.bukkit.OfflinePlayer;
