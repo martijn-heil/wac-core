f403cc6b11764256fff1de9f561a28d67d8d56c7
martijn-heil <m.heil375@gmail.com>
2017-01-08 15:49:36 +0100

    Disable enderpearling

--- src/main/kotlin/tk/martijn_heil/wac_core/GeneralListener.kt
+++ src/main/kotlin/tk/martijn_heil/wac_core/GeneralListener.kt
@@ -18,12 +18,16 @@
 
 package tk.martijn_heil.wac_core
 
+import org.bukkit.ChatColor
+import org.bukkit.Material
 import org.bukkit.entity.Player
 import org.bukkit.entity.SkeletonHorse
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.block.Action
 import org.bukkit.event.entity.CreatureSpawnEvent
 import org.bukkit.event.entity.EntityTargetEvent
+import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.event.player.PlayerJoinEvent
 
 
@@ -49,6 +53,15 @@ class GeneralListener() : Listener {
     fun onCreateSpawn(e: CreatureSpawnEvent) {
         if(e.spawnReason == CreatureSpawnEvent.SpawnReason.LIGHTNING && e.entity is SkeletonHorse) {
             e.isCancelled = true
+        }
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    fun onEnderPearl(e: PlayerInteractEvent) {
+        if(e.action == Action.RIGHT_CLICK_AIR || e.action == Action.RIGHT_CLICK_BLOCK
+                && e.player.inventory.itemInMainHand.type == Material.ENDER_PEARL) {
+            e.isCancelled = true
+            e.player.sendMessage(ChatColor.RED.toString() + "Ender pearls zijn uitgeschakeld!")
         }
     }
 }
